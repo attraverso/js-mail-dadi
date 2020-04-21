7e9d05f39d99bc9e4b98ef9b572df99502f7f3c5
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-04-21 17:31:57 +0200

    dadi complete except isNan check

---
+++ dadi/main.js
@@ -0,0 +1,21 @@
+/*get number btw 1-6 from player*/
+var giocatore1 = parseInt(prompt('scegli un numero da 1 a 6'));
+console.log('output g1', giocatore1);
+
+/*get random number btw 1-6 to play against*/
+var giocatore2 = Math.ceil(Math.random() * 6);
+console.log('output pc', giocatore2);
+
+/*check player number*/
+if ( /*isNan(giocatore1) == true ||*/ giocatore1 < 0 || giocatore1 > 6) {
+  document.writeln('Il valore inserito non è valido');
+} else {
+  /*determine winner*/
+  if (giocatore1 > giocatore2) {
+    console.log('giocatore 1 vince');
+  } else if (giocatore1 == giocatore2) {
+    console.log('parità');
+  } else {
+    console.log('giocatore 2 vince');
+  }
+}
