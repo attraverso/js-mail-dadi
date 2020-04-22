f40c5c75131fd39661e51f1de5e6e7ec61273a57
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-04-22 10:03:01 +0200

    comments added

--- mail/main.js
+++ mail/main.js
@@ -10,16 +10,17 @@ var allowed_emails = [
 /*prompt user to give own addresses*/
 var userEmail = prompt('Il tuo indirizzo email');
 
+/*define test variable*/
 var email_match = false;
 
-/*check against allowed addresses & show findings */
+/*check against allowed addresses*/
 for (var i = 0; i < allowed_emails.length; i++) {
-
   if (userEmail == allowed_emails[i]) {
     email_match = true;
   }
 }
 
+/*show findings*/
 if (email_match == true) {
   document.writeln('access granted');
 } else {
