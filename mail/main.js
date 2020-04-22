a9ce0f78238f8fd7a8e15cd65a6bead7bef884ae
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-04-22 09:39:44 +0200

    mail complete no checks

---
+++ mail/main.js
@@ -0,0 +1,27 @@
+/*define allowed email addresses*/
+var allowed_emails = [
+  "mail001@mail.com",
+  "mail002@mail.com",
+  "mail003@mail.com",
+  "mail004@mail.com",
+  "mail005@mail.com",
+]
+
+/*prompt user to give own addresses*/
+var userEmail = prompt('Il tuo indirizzo email');
+
+var email_match = false;
+
+/*check against allowed addresses & show findings */
+for (var i = 0; i < allowed_emails.length; i++) {
+
+  if (userEmail == allowed_emails[i]) {
+    email_match = true;
+  }
+}
+
+if (email_match == true) {
+  document.writeln('access granted');
+} else {
+  document.writeln('access denied');
+}
